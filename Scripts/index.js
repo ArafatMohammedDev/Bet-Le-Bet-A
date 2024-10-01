415aefbf6c6ea4df83c14822f4f4b00ed9c98502
Lewi Zelalem~ <ttfalewi@gmail.com>
2024-10-01 21:20:07 +0300

    #preLoader

---
+++ Scripts/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+ * PRELOAD
+ */
+
+const preloader = document.querySelector("[data-preload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
